1a05c04b50b9e5ebf70b3653148bad8651d18a18
anzalean <zablovskaya04@gmail.com>
2025-01-08 21:16:22 +0200

    add user, water, session models

---
+++ src/db/models/user.js
@@ -0,0 +1,63 @@
+import { Schema, model } from 'mongoose';
+
+const userShema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+    },
+    avatarURL: {
+      type: String || null,
+      default: null,
+    },
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+    },
+    gender: {
+      type: String || null,
+      enum: ['Man', 'Woman'],
+      default: null,
+    },
+    weight: {
+      type: Number,
+      default: 0,
+    },
+    activityTime: {
+      type: Number,
+      default: 0,
+    },
+    desiredVolume: {
+      type: Number,
+      min: 50,
+      max: 5000,
+      default: 1500,
+    },
+    accessToken: {
+      type: String,
+      default: '',
+    },
+    refreshToken: {
+      type: String,
+      default: '',
+    },
+    verification: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+export const User = model('User', userShema);
