9eff1af1e101113e940bed9dadabb7add0f1285b
Anatolii_Tomniak <anatoly.tomnyak@gmail.com>
2025-01-09 23:25:47 +0100

    add mail services

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,6 +3,9 @@ import {
   loginUserService,
   refreshUsersSessionService,
   logoutUserService,
+  requestResetTokenService,
+  resetPasswordService,
+  validateResetTokenService
 } from '../services/auth.js';
 
 const setupSession = (res, session) => {
@@ -79,3 +82,39 @@ export const logoutUserController = async (req, res) => {
   res.clearCookie('refreshToken');
   res.status(204).send();
 };
+
+//--------------------resetMailController--------------------
+export const requestResetEmailController = async (req, res) => {
+  await requestResetTokenService(req.body.email);
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+//--------------------validateResetTokenController--------------------
+export const validateResetTokenController = async (req, res) => {
+    const { token } = req.query;
+    const decodedToken = await validateResetTokenService({ token });
+
+    res.status(200).json({
+        status: 200,
+        message: "Token is valid",
+        data: {
+            decodedToken
+        }
+    });
+};
+
+//--------------------resetPasswordController--------------------
+export const resetPasswordController = async (req, res) => {
+    await resetPasswordService(req.body);
+    res.json({
+        message: 'Password was successfully reset!',
+        status: 200,
+        data: {}
+    });
+};
+
+
