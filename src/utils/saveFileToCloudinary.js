ecdc69f558eee998a0c7f09efbad71e96b955310
anzalean <zablovskaya04@gmail.com>
2025-01-10 15:27:35 +0200

    fixed imports

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,7 +1,7 @@
 import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
 
-import { CLOUDINARY } from '../constants/index.js';
+import { CLOUDINARY } from '../constants/constants.js';
 
 
 cloudinary.v2.config({
