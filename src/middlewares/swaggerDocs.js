29c784d0462ddfc80760d05d6cf98ee1e8a9705c
Anatolii_Tomniak <anatoly.tomnyak@gmail.com>
2025-01-15 17:56:15 +0100

    complited tasks discussed

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -4,10 +4,20 @@ import fs from 'node:fs';
 
 import { SWAGGER_PATH } from '../constants/constants.js';
 
+const options = {
+  explorer: true,
+  docExpansion: 'none', // Відображення описів (none, list, full)
+  displayOperationId: true, // Відображення operationId для кожного маршруту
+  defaultModelsExpandDepth: 1, // Відображення схем за замовчуванням
+  defaultModelExpandDepth: 1, // Рівень розширення моделей
+  defaultModelRendering: 'model', // Відображення моделі чи коду прикладу
+  displayRequestDuration: true, // Відображення тривалості запиту
+  };
+
 export const swaggerDocs = () => {
   try {
     const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
+    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc, options)];
   } catch (err) {
     return (req, res, next) =>
       next(createHttpError(500, "Can't load swagger docs", { error: err }));
