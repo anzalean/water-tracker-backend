d7190913ddf829c8d282c6098022c9fc588ccec6
anzalean <zablovskaya04@gmail.com>
2025-01-20 17:06:21 +0200

    fixed token

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -21,8 +21,8 @@ export const SMTP = {
     SMTP_FROM: env('SMTP_FROM'),
 };
 
-// export const accessTokenLifeTime = 1000 * 60 * 15; // 15 minutes
-export const accessTokenLifeTime = 1000 * 60 * 60 * 3; // 3 hours
+export const accessTokenLifeTime = 1000 * 60 * 15; // 15 minutes
+// export const accessTokenLifeTime = 1000 * 60 * 60 * 3;
 export const refreshTokenLifeTime = 1000 * 60 * 60 * 24 * 30; // 30 days
 
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
